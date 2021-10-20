92044d7886ba03845b46a209d427d157f464b862
AnjaliMohan-U <anjalimohan.uppu@cjsstechnologies.com>
2021-10-20 16:31:43 +0530

    calculator service

---
+++ src/main/java/com/example/employee/controller/CalculatorController.java
@@ -0,0 +1,21 @@
+package com.example.employee.controller;
+
+import com.example.employee.service.CalculatorService;
+
+public class CalculatorController {
+    private CalculatorService calserv;
+
+    public int add(int i1, int i2){
+        return i1+i2;
+    }
+    public int subtract(int i1, int i2){
+        return i1-i2;
+    }
+    public int divide(int i1, int i2){
+        return i1-i2;
+    }
+    public int multiply(int i1, int i2){
+        return  i1-i2;
+    }
+
+}
---
+++ src/main/java/com/example/employee/service/CalculatorService.java
@@ -0,0 +1,9 @@
+package com.example.employee.service;
+
+public interface CalculatorService {
+    public int add(int i1, int i2);
+    public int subtract(int i1, int i2);
+    public int divide(int i1, int i2);
+    public int multiply(int i1, int i2);
+
+}
