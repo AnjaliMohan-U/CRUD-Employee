b9b4ab2f866719e8092f9f0ceede9a408a4482fa
AnjaliMohan-U <anjalimohan.uppu@cjsstechnologies.com>
2021-10-20 14:28:24 +0530

    added new

--- src/test/java/com/example/employee/service/PracticeServiceTest.java
+++ src/test/java/com/example/employee/service/PracticeServiceTest.java
@@ -3,23 +3,49 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
-
+import org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class PracticeServiceTest {
+
     @InjectMocks
     private PracticeService practiceService;
 
+    int total_employees = 0;
+
     @Before
-    public void before(){
-        System.out.println("Before");
+    public void setData(){
+        System.out.println("total employees Before: "+ this.total_employees);
+        this.total_employees = 9;
     }
     @After
     public void after(){
-        System.out.println("After");
+        System.out.println("total employees After: "+ this.total_employees);
+        System.out.println(this.total_employees);
     }
+
+    @Test
+    public void sumOfThree(){
+        System.out.println("Test1");
+        assertEquals("HELLO",6, practiceService.sum(new int[] {4,1,1}));
+    }
+
+    @Test
+    public void testString(){
+        String name = "My name is Anjali";
+        assertThat(name).as("some of the testcases didn't match")
+                .isEqualTo("I am anjali")
+                .isEqualToIgnoringCase("I am anjali")
+                .startsWith("I")
+                .endsWith("!")
+                .containsIgnoringCase("anjali");
+    }
+
     @BeforeClass
     public static void beforeClass() {
         System.out.println("Before Class");
@@ -29,9 +55,4 @@ public class PracticeServiceTest {
         System.out.println("After Class");
     }
 
-    @Test
-    public void sumOfThree(){
-        System.out.println("Test1");
-        assertEquals(6, practiceService.sum(new int[] {4,1,1}));
-    }
 }
